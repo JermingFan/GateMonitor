c9157e3ef53c4132f99bcf0bf844910bbbd29c54
JermingFan <jermingfan@foxmail.com>
2016-02-27 11:09:19 +0800

    ALL

---
+++ GateMonitor/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace GateMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("宋体", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(196, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(476, 100);
+            this.label1.TabIndex = 2;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Location = new System.Drawing.Point(196, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(476, 100);
+            this.label2.TabIndex = 3;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(678, 30);
+            this.label4.Name = "label4";
+            this.tableLayoutPanel1.SetRowSpan(this.label4, 4);
+            this.label4.Size = new System.Drawing.Size(284, 400);
+            this.label4.TabIndex = 5;
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.Location = new System.Drawing.Point(196, 230);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(476, 100);
+            this.label3.TabIndex = 4;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 1, 4);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(965, 544);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.tableLayoutPanel1.SetColumnSpan(this.dataGridView1, 3);
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 433);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.Size = new System.Drawing.Size(959, 208);
+            this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.DataBindingComplete += new System.Windows.Forms.DataGridViewBindingCompleteEventHandler(this.dataGridView1_DataBindingComplete);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.tableLayoutPanel1.SetRowSpan(this.pictureBox1, 4);
+            this.pictureBox1.Size = new System.Drawing.Size(187, 394);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "0"});
+            this.comboBox1.Location = new System.Drawing.Point(678, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(284, 20);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(196, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(476, 30);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "门禁编号：";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.Location = new System.Drawing.Point(196, 330);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(476, 100);
+            this.label6.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(965, 544);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "门禁监视器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ GateMonitor/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GateMonitor
+{
+    public partial class Form1 : Form
+    {
+        private string FConnStr;
+        private string FAvatarPath;
+        private string FAvatarExtension;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            var s = new Settings1();
+            FConnStr = s.ConnStr;
+            FAvatarPath = s.AvatarPath;
+            FAvatarExtension = s.AvatarExtension;
+            FGate = s.DefaultGate;
+            
+        }
+
+        private void dataGridView1_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            if (dataGridView1.RowCount > 0)
+            {
+                dataGridView1.Columns[1].Visible = false;
+                label1.Text = (string)dataGridView1.Rows[0].Cells["username"].Value;
+                label2.Text = (string)dataGridView1.Rows[0].Cells["job"].Value;
+                label3.Text = (string)dataGridView1.Rows[0].Cells["company"].Value;
+                label4.Text = (string)dataGridView1.Rows[0].Cells["dir"].Value;
+                switch (label4.Text)
+                {
+                    case "进":
+                        label4.BackColor = Color.Yellow;
+                        break;
+                    case "出":
+                        label4.BackColor = Color.Cyan;
+                        break;
+                }
+                pictureBox1.ImageLocation = FAvatarPath + ((string)dataGridView1.Rows[0].Cells["id_card"].Value).Trim() + FAvatarExtension;
+                label6.Text = (string)dataGridView1.Rows[0].Cells["edu_time"].Value;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                var conn = new MySql.Data.MySqlClient.MySqlConnection(FConnStr);
+                conn.Open();
+
+                var da = new MySql.Data.MySqlClient.MySqlDataAdapter("SELECT IF(u.user_name IS NULL, '', u.user_name) AS username, IF(u.edu_time IS NULL, '', u.edu_time) AS edu_time, IF(u.user_identity IS NULL, '1', u.user_identity) AS id_card, IF(i.dotime IS NULL, '0000-00-00 00:00:00', i.dotime) AS time, IF(i.flag=0, '进', '出') AS dir, IF(j.job_name IS NULL, '', j.job_name) AS job, IF(c.company_name IS NULL, '', c.company_name) AS company FROM iolog i, user u, job j, company c WHERE i.gate = " + FGate + " AND i.card_id = u.card_id AND u.job_id = j.job_id AND j.company_id = c.company_id ORDER BY i.dotime DESC, u.user_name ASC LIMIT 30", conn);
+                var ds = new DataSet();
+                da.Fill(ds);
+                DateTime time = new DateTime();
+                this.dataGridView1.AutoGenerateColumns = true;
+                //添加的地方
+                int m = 0;
+                int n = 0;
+                int h = ds.Tables[0].Rows.Count;
+                //  Console.Write(a);
+                for (m = 0; m < h; m++)
+                {
+                    for (n = m + 1; n < h; n++)
+                        if (Convert.ToString(ds.Tables[0].Rows[m][0]) == Convert.ToString(ds.Tables[0].Rows[n][0]) && Convert.ToString(ds.Tables[0].Rows[m][4]) == Convert.ToString(ds.Tables[0].Rows[n][4]))
+                        {
+                            TimeSpan a = Convert.ToDateTime(ds.Tables[0].Rows[m][3]) - Convert.ToDateTime(ds.Tables[0].Rows[n][3]);
+                            double b = a.TotalSeconds;
+                            if (b > 0 && b < 1000)
+                            {
+                                ds.Tables[0].Rows.RemoveAt(m);
+                                //  Console.Write(ds.Tables[0].Rows[0][0]);
+                                //  m = m - 1;
+                                n = 0;
+                                h = h - 1;
+                            }
+                            //Console.Write("大");
+                            // ds.Tables[0].Rows.RemoveAt(0);
+                        }
+                    //m = m - 1;
+                }
+                this.dataGridView1.DataSource = ds.Tables[0];
+                this.dataGridView1.AutoResizeColumns();
+                this.dataGridView1.AutoResizeRows();
+                int sum = 0;
+                int i = 0;
+                for (i = 2; i < dataGridView1.Columns.Count; ++i)
+                {
+                    sum += dataGridView1.Columns[i].Width;
+                }
+                dataGridView1.Columns[0].Width = dataGridView1.Width - sum;
+
+                da.Dispose();
+                conn.Close();
+                conn.Dispose();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        public Boolean SetFormFullScreen(Boolean Fullscreen, ref Rectangle OldRect)
+        {
+            Int32 hwnd = 0;
+            hwnd = FindWindow("Shell_TrayWnd", null);
+
+            if (hwnd == 0) return false;
+            //this.SuspendLayout();
+            if (Fullscreen)
+            {
+                ShowWindow(hwnd, SW_HIDE);
+
+                SystemParametersInfo(SPI_GETWORKAREA, 0, ref OldRect, SPIF_UPDATEINIFILE);
+                Rectangle rectFull = Screen.PrimaryScreen.Bounds;
+                SystemParametersInfo(SPI_SETWORKAREA, 0, ref rectFull, SPIF_UPDATEINIFILE);
+
+                FormBorderStyle = FormBorderStyle.None;
+                WindowState = FormWindowState.Maximized;
+                Activate();
+            }
+            else
+            {
+                WindowState = FormWindowState.Normal;
+                FormBorderStyle = FormBorderStyle.Sizable;
+
+                ShowWindow(hwnd, SW_SHOW);
+
+                SystemParametersInfo(SPI_SETWORKAREA, 0, ref OldRect, SPIF_UPDATEINIFILE);
+
+                Activate();
+            }
+            return true;
+        }
+        #region user32.dll
+
+        [DllImport("user32.dll", EntryPoint = "ShowWindow")]
+        public static extern Int32 ShowWindow(Int32 hwnd, Int32 nCmdShow);
+        public const Int32 SW_SHOW = 5; public const Int32 SW_HIDE = 0;
+
+        [DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+        private static extern Int32 SystemParametersInfo(Int32 uAction, Int32 uParam, ref Rectangle lpvParam, Int32 fuWinIni);
+        public const Int32 SPIF_UPDATEINIFILE = 0x1;
+        public const Int32 SPI_SETWORKAREA = 47;
+        public const Int32 SPI_GETWORKAREA = 48;
+
+        [DllImport("user32.dll", EntryPoint = "FindWindow")]
+        private static extern Int32 FindWindow(string lpClassName, string lpWindowName);
+
+        #endregion
+
+        private bool FIsFullScreen = true;
+        private string FGate = "0";
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F11)
+            {
+                Rectangle r;
+                r = Rectangle.Empty;
+                FIsFullScreen = !FIsFullScreen;
+                this.SetFormFullScreen(FIsFullScreen, ref r);
+            }
+            else
+            {
+                MessageBox.Show("请按F11切换全屏/非全屏模式！");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form1_SizeChanged(sender, e);
+            if (FIsFullScreen)
+            {
+                Rectangle r;
+                r = Rectangle.Empty;
+                this.SetFormFullScreen(true, ref r);
+            }
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            label1.Font = new Font(label1.Font.FontFamily.Name, (int) (label1.Height * 0.8));
+            label2.Font = new Font(label2.Font.FontFamily.Name, (int) (label2.Height * 0.5));
+            label3.Font = new Font(label3.Font.FontFamily.Name, (int) (label3.Height * 0.5));
+            label4.Font = new Font(label4.Font.FontFamily.Name, (int) (label4.Height * 0.75));
+            label6.Font = new Font(label6.Font.FontFamily.Name, (int) (label6.Height * 0.8));
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FGate = comboBox1.Text;
+        }
+    }
+
+
+}
